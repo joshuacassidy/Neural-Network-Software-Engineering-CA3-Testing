99e3b653b5b04984c761ea6c1ef818e4237f50da
joshuacassidy <Joshcassidy79@gmail.com>
2018-04-27 20:17:34 +0100

    Added new test for the main class

--- Tests/com/josh/SoftwareEngineeringCA3/MainTest.java
+++ Tests/com/josh/SoftwareEngineeringCA3/MainTest.java
@@ -7,12 +7,13 @@ import org.junit.Test;
  * Created by Josh on 20/04/2018.
  */
 public class MainTest {
-
+    private Main main;
     private String[] args;
 
     @Before
     public void setUp() throws Exception {
         args = null;
+        main = new Main();
     }
 
     @Test
@@ -20,4 +21,8 @@ public class MainTest {
         Main.main(args);
     }
 
+    @Test
+    public void testMainWithNewInstance() throws Exception {
+        main.main(args);
+    }
 }
